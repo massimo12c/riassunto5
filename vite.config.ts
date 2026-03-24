b2e96bd80421d95b2a508fba70f80811baba550e
Massimo Martella <massimo.martella@tim.it>
2026-03-24 22:36:56 +0100

    fix final github pages path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/riassunto5/',
   plugins: [react()],
-  base: '/riassumi/', // Replace with your repo name if different
 })
